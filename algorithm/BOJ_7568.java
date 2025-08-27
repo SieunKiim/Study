82f7a0339f0c6c1fc6e014c211daaf0e693ab789
SieunKiim <sieun960513@naver.com>
2025-08-27 23:01:53 +0900

    algo

---
+++ algorithm/BOJ_7568.java
@@ -0,0 +1,6 @@
+public class BOJ_7568 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        
+    }
+}
