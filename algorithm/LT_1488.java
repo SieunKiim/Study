06630720b3dc319b88a90e4731829abf7523665a
SieunKiim <sieun960513@naver.com>
2025-10-08 16:50:23 +0900

    LT

---
+++ algorithm/LT_1488.java
@@ -0,0 +1,45 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.TreeSet;
+
+public class LT_1488 {
+    public static void main(String[] args) {
+        
+
+        class Solution {
+
+            public int[] avoidFlood(int[] rains) {
+                TreeSet<Integer> zeroPoint = new TreeSet<>();
+                HashMap<Integer, Integer> rainMap = new HashMap<>();
+                int[] output = new int[rains.length];
+                Arrays.fill(output, 1);
+
+                for (int i = 0; i < rains.length; i++) {
+                    if (rains[i] == 0) {
+                        zeroPoint.add(i);
+
+
+                    } else {
+                        output[i] = -1;
+                        
+
+                        if (rainMap.containsKey(rains[i])) {
+                            int lastPoint = rainMap.get(rains[i]);
+                            Integer nextZeroPoint = zeroPoint.higher(lastPoint);
+                            if (nextZeroPoint == null || nextZeroPoint > i)
+                                return new int[] {};
+                            output[nextZeroPoint] = rains[i];
+                            zeroPoint.remove(nextZeroPoint);
+                        }
+                        rainMap.put(rains[i], i);
+                    }
+                }
+                return output;
+            }
+        }
+        
+        Solution s = new Solution();
+        var temp = s.avoidFlood(new int[] { 1, 2, 0, 2, 1 });
+        System.out.println(Arrays.toString(temp));
+    }
+}
