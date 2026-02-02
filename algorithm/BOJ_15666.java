d7a2aae10cc06e9bfce56761e7902fc418274f33
SieunKiim <sieun960513@naver.com>
2026-02-03 00:22:36 +0900

    BOJ

--- algorithm/BOJ_15666.java
+++ algorithm/BOJ_15666.java
@@ -1,12 +1,40 @@
 import java.util.*;
+import java.util.stream.Collectors;
 import java.io.*;
 
 public class BOJ_15666 {
-    public static void main(String[] args) throws IOException{
+    private static Integer[] arr;
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
-        
+        // arr = new int[n];
+        TreeSet<Integer> set = new TreeSet<>();
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            set.add(Integer.parseInt(st.nextToken()));
+        }
+        // Arrays.sort(arr);
+        arr = set.toArray(new Integer[0]);
+        for (int i = 0; i < arr.length; i++) {
+            List<Integer> temp = new ArrayList<>();
+            temp.add(arr[i]);
+            dfs(temp, i, m - 1);
+            temp.remove(temp.size() - 1);
+        }
+    }
+    
+    private static void dfs(List<Integer> list, int index, int count) {
+        if (count == 0) {
+            System.out.println(list.stream().map(String::valueOf).collect(Collectors.joining(" ")));
+            return;
+        }
+        for (int i = index; i < arr.length; i++) {
+            list.add(arr[i]);
+            dfs(list, i, count - 1);
+            list.remove(list.size() - 1);
+        }
+
     }
 }
