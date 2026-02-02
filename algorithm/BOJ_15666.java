b0cdf7b7c007148539927cb7a43fcb31f9fef16e
SieunKiim <sieun960513@naver.com>
2026-02-02 23:50:15 +0900

    boj

---
+++ algorithm/BOJ_15666.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+
+public class BOJ_15666 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        
+    }
+}
