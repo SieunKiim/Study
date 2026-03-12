03f0fef192220f5930ddcffd88357779ab864bb8
SieunKiim <sieun960513@naver.com>
2026-03-12 20:33:19 +0900

    BOJ

---
+++ algorithm/BOJ_9655.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+
+public class BOJ_9655 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        if(n%2 ==0){
+            System.out.println("CY");
+        } else {
+            System.out.println("SK");
+        }
+    }
+}
